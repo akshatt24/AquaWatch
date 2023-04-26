03876a2a8dc4f73f17333d8d2e6f9f0ab065e455
Akshat Dhingra <127871424+akshatt24@users.noreply.github.com>
2023-04-26 19:59:52 +0530

    Add files via upload

---
+++ AquaWatch/Aquawatch/includes/css_header.php
@@ -0,0 +1,5 @@
+<head>
+	<title>JooteKhareedo | The home of shoes</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
